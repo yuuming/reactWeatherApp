de84fa50731f8f3e547946d5facb095314d96538
Yuumi <yuuming.n@gmail.com>
2018-10-23 16:45:26 -0700

    Create Chart.js to separate a component

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,25 +1,23 @@
 import React, { Component } from "react";
-import { connect } from 'react-redux';
-import { Sparklines, SparklinesLine } from 'react-sparklines';
+import { connect } from "react-redux";
+import Chart from "../components/chart";
 
 class WeatherList extends Component {
-    renderWeather(cityData) {
-        console.log(cityData);
-        const name = cityData.city.name;
-       const temps = cityData.list.map(weather => weather.main.temp);
-       console.log('list!!!!', cityData.list);
-       console.log(temps);
-        return (
-            <tr key ={name}>
-                <td>{name}</td>
-                <td>
-                <Sparklines height={120} width={180} data={temps}>
-                <SparklinesLine color="red" />
-                </Sparklines>
-                </td>
-            </tr>
-        );
-    }
+  renderWeather(cityData) {
+    console.log(cityData);
+    const name = cityData.city.name;
+    const temps = cityData.list.map(weather => weather.main.temp);
+    console.log("list!!!!", cityData.list);
+    console.log(temps);
+    return (
+      <tr key={name}>
+        <td>{name}</td>
+        <td>
+          <Chart data={temps} color="orange" />
+        </td>
+      </tr>
+    );
+  }
   render() {
     return (
       <table className="table table-hover">
@@ -31,16 +29,14 @@ class WeatherList extends Component {
             <th>Humidity</th>
           </tr>
         </thead>
-        <tbody>
-            {this.props.weather.map(this.renderWeather)}
-        </tbody>
+        <tbody>{this.props.weather.map(this.renderWeather)}</tbody>
       </table>
     );
   }
 }
 
 function mapStateToProps({ weather }) {
-    return { weather }; // { weather } === { weather : weather}
+  return { weather }; // { weather } === { weather : weather}
 }
 
 export default connect(mapStateToProps)(WeatherList);
