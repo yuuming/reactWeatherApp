b1d62e487311db413ccc971ef98884a8a39e242b
Yuumi <yuuming.n@gmail.com>
2018-10-24 12:55:18 -0700

    Implement google map for the location

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import Chart from "../components/chart";
+import GoogleMap from '../components/google.map';
 
 class WeatherList extends Component {
   renderWeather(cityData) {
@@ -9,14 +10,15 @@ class WeatherList extends Component {
     const temps = cityData.list.map(weather => weather.main.temp);
     const pressure = cityData.list.map(weather => weather.main.pressure);
     const humidities = cityData.list.map(weather => weather.main.humidity);
-
+    // const lon = cityData.city.coord.lon; we can make it short to next line
+    const { lon, lat } = cityData.city.coord;
 
 
     console.log("list!!!!", cityData.list);
     console.log(temps);
     return (
       <tr key={name}>
-        <td>{name}</td>
+        <td><GoogleMap lon={lon} lat={lat} /></td>
         <td><Chart data={temps} color="orange" units="K" /></td>
         <td><Chart data={pressure} color="black" units="hPa" /></td>
 
